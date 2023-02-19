f8d7b4e1b131e122e802bdb02146069982350b60
Roberto Butti <roberto.butti@gmail.com>
2023-02-19 08:54:38 +0100

    added dump method for Data

--- src/Data/BasicData.php
+++ src/Data/BasicData.php
@@ -13,6 +13,13 @@ class BasicData
         $this->response = $response;
     }
 
+    public function dump()
+    {
+        foreach ($this->response as $key => $item) {
+            var_dump($key, $item);
+        }
+    }
+
     public function getCollectionFromResponse($name): Collection
     {
         if (! array_key_exists($name, $this->response)) {
