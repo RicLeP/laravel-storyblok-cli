45f20297642ef2a1acf8045662cca5d066b0ef4d
Richard Le Poidevin <ric@sirric.co.uk>
2023-03-14 10:53:44 +0000

    Stories endpoints

---
+++ src/Endpoints/Stories.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Riclep\StoryblokCli\Endpoints;
+
+use Riclep\StoryblokCli\Data\StoriesData;
+
+class Stories extends BasicEndpoint
+{
+    public function __construct($token)
+    {
+        parent::__construct($token);
+    }
+
+    public static function make($token = null): self
+    {
+        return new self(parent::getToken($token));
+    }
+
+    public function byId($storyId): StoriesData
+    {
+        return new StoriesData(
+            $this->client->get('stories/' . $storyId)->getBody()
+        );
+    }
+
+	public function all(): StoriesData
+	{
+		return new StoriesData(
+			$this->client->get('stories')->getBody()
+		);
+	}
+
+	public function search($search): StoriesData
+	{
+		return new StoriesData(
+			$this->client->get('stories/?text_search=' . urlencode($search))->getBody()
+		);
+	}
+
+	public function create($story): StoriesData {
+		if (is_array($story)) {
+			$story = json_encode($story);
+		}
+
+		return new StoriesData(
+			$this->client->post('stories', $story)->getBody()
+		);
+	}
+
+	public function update($id, $story): StoriesData {
+		if (is_array($story)) {
+			$story = json_encode($story);
+		}
+
+		return new StoriesData(
+			$this->client->put('stories/' . $id, $story)->getBody()
+		);
+	}
+
+	public function publish($id): StoriesData {
+		return new StoriesData(
+			$this->client->get('stories/' . $id . '/publish')->getBody()
+		);
+	}
+
+	public function unpublish($id): StoriesData {
+		return new StoriesData(
+			$this->client->get('stories/' . $id . '/unpublish')->getBody()
+		);
+	}
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,13 +1,11 @@
 <?php
 
 use Riclep\StoryblokCli\Endpoints\Spaces;
+use Riclep\StoryblokCli\Endpoints\Stories;
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
 
-test('mock', function () {
-    $spaces = Spaces::make('aaaa');
+test('can get spaces', function () {
+    $spaces = Spaces::make('dummy-token');
     $spaces->mockable([
         mockResponse('spaces'),
     ]);
@@ -18,3 +16,125 @@ test('mock', function () {
     expect($spaceValues[0]['name'])->toEqual('Example Space 1');
     expect($spaceValues[1]['name'])->toEqual('Example Space 2');
 });
+
+test('can get a single story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->byId(259645264);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
+
+test('can get stories', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories'),
+	]);
+
+	$storiesData = $stories->all();
+
+	expect($storiesData->getStories())->not()->toBeNull();
+	expect($storiesData->getStories())->toHaveCount(3);
+
+	expect($storiesData->getStories()[0]['name'])->toEqual('Test story');
+});
+
+
+
+test('can search stories', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/search-results'),
+	]);
+
+	$storiesData = $stories->search('test');
+
+	expect($storiesData->getStories())->not()->toBeNull();
+	expect($storiesData->getStories())->toHaveCount(1);
+
+	expect($storiesData->getStories()[0]['name'])->toEqual('Test story');
+});
+
+test('can create a story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->create([
+		'name' => 'Home',
+		'slug' => 'home',
+		'content' => [
+			'component' => 'page',
+			'body' => []
+		],
+		'publish' => 1
+	]);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
+test('can update a story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->update(259645264, [
+		'name' => 'Home',
+		'slug' => 'home',
+		'content' => [
+			'component' => 'page',
+			'body' => []
+		],
+		'publish' => 1
+	]);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
+test('can delete a story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->update(259645264, [
+		'name' => 'Home',
+		'slug' => 'home',
+		'content' => [
+			'component' => 'page',
+			'body' => []
+		],
+		'publish' => 1
+	]);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
+test('can publish a story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->publish(259645264);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
+test('can unpublish a story', function () {
+	$stories = Stories::make('dummy-token');
+	$stories->mockable([
+		mockResponse('stories/259645264'),
+	]);
+
+	$storiesData = $stories->unpublish(259645264);
+
+	expect($storiesData->getStory()['name'])->toEqual('Home');
+});
+
